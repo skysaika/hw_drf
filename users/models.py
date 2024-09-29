bc7f4932af4f02bd9ca7bdee7b17d6fca7817cd9
saysuu <skysaika@yandex.ru>
2024-09-29 22:55:56 +0300

    Add static and media, expand model User(phone, city, avatar) and change authorization to email, makemigrations and migrate

--- users/models.py
+++ users/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class User(AbstractUser):
-    pass
+    """Модель пользователя"""
+    username = None  # поле удалится
+    # заменим его на email, который нужно переопределить:
+    email = models.EmailField(unique=True, verbose_name='почта')  # поле уникальное
+
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    city = models.CharField(max_length=35, verbose_name='город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []

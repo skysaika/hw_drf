380c78ec28ac2c35c37cb4e4f7832ed7ae5b7885
saysuu <skysaika@yandex.ru>
2024-10-09 21:50:34 +0300

    Add class LessonSerializer in app_study/serializers.py.

--- app_study/serializers.py
+++ app_study/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from app_study.models import Course
+from app_study.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор курса"""
     class Meta:
         model = Course
+        fields = '__all__'
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    """Сериализатор урока"""
+    class Meta:
+        model = Lesson
         fields = '__all__'

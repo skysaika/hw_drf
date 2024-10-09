0cf3c8218ae0200df3a3be704ce5be35d1feb333
saysuu <skysaika@yandex.ru>
2024-10-09 21:55:09 +0300

    Add CRUD: class LessonCreateAPIView in app_study/views.py, path to lesson-create in app_study/urls.py and test POST in postman: http://localhost:8000/lesson/create/

--- app_study/urls.py
+++ app_study/urls.py
@@ -1,12 +1,15 @@
+from django.urls import path
+
 from app_study.apps import AppStudyConfig
 from rest_framework.routers import DefaultRouter
 
-from app_study.views import CourseViewSet
+from app_study.views import CourseViewSet, LessonCreateAPIView
 
 app_name = AppStudyConfig.name
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
-urlpatterns = [
 
+urlpatterns = [
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
 ] + router.urls
--- app_study/views.py
+++ app_study/views.py
@@ -1,10 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from app_study.models import Course
-from app_study.serializers import CourseSerializer
+from app_study.serializers import CourseSerializer, LessonSerializer
 
 
+# на основе вьюсета ModelViewSet
 class CourseViewSet(viewsets.ModelViewSet):
-    """Вьюсет курсов"""
+    """Представление для курса на основе вьюсета"""
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+
+# на основе дженериков по CRUD для Generic
+class LessonCreateAPIView(generics.CreateAPIView):
+    """Представление для создания урока на основе дженериков"""
+    serializer_class = LessonSerializer

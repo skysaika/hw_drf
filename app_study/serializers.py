d1f4ba3334e238b9f29b270c7501f046a800ddf3
saysuu <skysaika@yandex.ru>
2025-03-20 16:31:35 +0300

    Add field lesson_count and method def_lesson_count in CourseSerializer

--- app_study/serializers.py
+++ app_study/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
-
-from app_study.models import Course, Lesson
+from .models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор курса"""
+    lesson_count = serializers.SerializerMethodField()  # поле для вывода количества уроков
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()  # возвращает количество уроков
 
 
 class LessonSerializer(serializers.ModelSerializer):

fc1ebe6e3ab50310d6f6fe5e67238a472fdaf7d1
saya <skysaika@yandex.ru>
2025-04-07 13:54:15 +0300

    Put LessonSerializer higher than CourseSerializer and add lesson info display in CourseSerializer

--- app_study/serializers.py
+++ app_study/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import Course, Lesson
 
+class LessonSerializer(serializers.ModelSerializer):
+    """Сериализатор урока"""
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
 
 class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор курса"""
     lesson_count = serializers.SerializerMethodField()  # поле для вывода количества уроков
+    lessons = LessonSerializer(many=True, read_only=True)  # поле для вывода уроков
+
 
     class Meta:
         model = Course
@@ -12,10 +20,3 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_lesson_count(self, obj):
         return obj.lessons.count()  # возвращает количество уроков
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    """Сериализатор урока"""
-    class Meta:
-        model = Lesson
-        fields = '__all__'

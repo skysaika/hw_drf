333b8be0c1e4ed043c32cee480fe81f545eb1520
saya <skysaika@yandex.ru>
2025-04-12 19:24:31 +0300

    Change permissions.BasePermission to BasePermission in permissions.py

--- app_study/permissions.py
+++ app_study/permissions.py
@@ -1,17 +1,18 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
-class IsOwner(permissions.BasePermission):
+
+class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
 
-class IsModerator(permissions.BasePermission):
+class IsModerator(BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name='moderators').exists()
 
-class NotModerator(permissions.BasePermission):
+class NotModerator(BasePermission):
     def has_permission(self, request, view):
         return not request.user.groups.filter(name='moderators').exists()
 
-class IsOwnerOrModerator(permissions.BasePermission):
+class IsOwnerOrModerator(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user or request.user.groups.filter(name='moderators').exists()

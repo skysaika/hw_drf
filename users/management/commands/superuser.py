23f48641ee5ce6a927024c5fbb57ddbd7640a1fc
saya <skysaika@yandex.ru>
2025-04-12 16:26:21 +0300

    Change suser command, add SUPERUSER_PASSWORD to .env and .env.example, add djangorestframework_simplejw to requirements.txt

--- users/management/commands/superuser.py
+++ users/management/commands/superuser.py
@@ -1,20 +1,34 @@
 from django.core.management import BaseCommand
+from dotenv import load_dotenv
+import os
 
 from users.models import User
 
 
 class Command(BaseCommand):
+    help = "Создать нового суперпользователя"
 
     def handle(self, *args, **options):
+        email = 'admin@sky.pro'
+        password = os.getenv("SUPERUSER_PASSWORD")
 
+        # Удаление существующего суперпользователя (если он существует)
+        try:
+            user = User.objects.get(email=email)
+            user.delete()
+            self.stdout.write(self.style.SUCCESS(f"Суперпользователь с email {email} успешно удален"))
+        except User.DoesNotExist:
+            self.stdout.write(self.style.WARNING(f"Суперпользователь с email {email} не найден"))
+
+        # Создание нового суперпользователя
         user = User.objects.create(
-            email='admin@sky.pro',  # после создания нужно входить через этот адрес
+            email=email,
             first_name='Admin',
             last_name='SkyPro',
             is_staff=True,
             is_superuser=True,
         )
-        # зададим пароль
-        user.set_password('123qwe')
-        # сохраняем
+        user.set_password(password)
         user.save()
+
+        self.stdout.write(self.style.SUCCESS(f"Новый суперпользователь с email {email} успешно создан"))

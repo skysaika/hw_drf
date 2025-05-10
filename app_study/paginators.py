6102baaf1b4ce898409e1acab640c5e28cd5b23c
saya <skysaika@yandex.ru>
2025-05-10 14:57:20 +0300

    Create paginators.py, add Coursepaginator, LessonPaginator, PaymentPaginator in views.py

---
+++ app_study/paginators.py
@@ -0,0 +1,15 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+
+class CoursePaginator(PageNumberPagination):
+    """Пагинатор для курса"""
+    page_size = 2
+
+class LessonPaginator(CoursePaginator):
+    """Пагинатор для уроков"""
+    page_size = 5
+
+class PaymentPaginator(CoursePaginator):
+    """Пагинатор для списка платежей"""
+    page_size = 2

1091daf1eaa0fe0deecf9f6f9ae230ee9de2780e
saya <skysaika@yandex.ru>
2025-06-24 18:47:18 +0300

    Change class LessonUpdateAPIView in views.py

--- app_study/views.py
+++ app_study/views.py
@@ -13,6 +13,7 @@ from app_study.paginators import CoursePaginator, LessonPaginator, PaymentPagina
 from app_study.permissions import IsModerator, IsOwner, NotModerator, IsOwnerOrModerator
 from app_study.serializers import CourseSerializer, LessonSerializer, PaymentSerializer, CourseSubscriptionSerializer
 from app_study.services import create_payment_intent, retrieve_payment_intent
+from app_study.tasks import send_course_update_emails
 
 
 # на основе вьюсета ModelViewSet
@@ -85,7 +86,7 @@ class LessonRetrieveAPIView(generics.RetrieveAPIView):
 
 
 
-class LessonUpdateAPIView(generics.UpdateAPIView):  # поддерживает как  PUT так и PATCH
+class LessonUpdateAPIView(generics.UpdateAPIView):  # поддерживает как PUT, так и PATCH
     """Представление для обновления урока на основе дженериков"""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
@@ -93,13 +94,29 @@ class LessonUpdateAPIView(generics.UpdateAPIView):  # поддерживает �
 
     def patch(self, request, *args, **kwargs):
         print(
-            f"Обновление урока: Пользователь - {self.request.user}, Супер - {self.request.user.is_superuser}, Модератор - {self.request.user.groups.filter(name='moderators').exists()}")
-        return super().patch(request, *args, **kwargs)
+            f"Обновление урока: Пользователь - {request.user}, Супер - {request.user.is_superuser}, Модератор - {request.user.groups.filter(name='moderators').exists()}"
+        )
+        response = super().patch(request, *args, **kwargs)
+
+        lesson = self.get_object()
+        if lesson.course:
+            print(f'Calling send_course_update_emails for course ID: {lesson.course.id}')
+            send_course_update_emails.delay(lesson.course.id)
+
+        return response
 
     def put(self, request, *args, **kwargs):
         print(
-            f"Обновление урока: Пользователь - {self.request.user}, Супер - {self.request.user.is_superuser}, Модератор - {self.request.user.groups.filter(name='moderators').exists()}")
-        return super().put(request, *args, **kwargs)
+            f"Обновление урока: Пользователь - {request.user}, Супер - {request.user.is_superuser}, Модератор - {request.user.groups.filter(name='moderators').exists()}"
+        )
+        response = super().put(request, *args, **kwargs)
+
+        lesson = self.get_object()
+        if lesson.course:
+            print(f'Calling send_course_update_emails for course ID: {lesson.course.id}')
+            send_course_update_emails.delay(lesson.course.id)
+
+        return response
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):  # поддерживает только DELETE
